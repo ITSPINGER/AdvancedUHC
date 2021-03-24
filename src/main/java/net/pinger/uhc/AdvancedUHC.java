5099d298e33074501554ba7b4fd90e0f46cb873b
Dimitrije Mijailovic <pinger.business@gmail.com>
2021-03-24 16:33:03 +0100

    Initializing the project

---
+++ src/main/java/net/pinger/uhc/AdvancedUHC.java
@@ -0,0 +1,11 @@
+package net.pinger.uhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AdvancedUHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
